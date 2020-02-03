ea4751edc6bf567e35f48f0bddc5e8d8f126c9ca
Khang Tran <khvitran@ucsc.edu>
2020-02-02 22:18:40 -0800

    updated all queries

--- Lab2/query4.sql
+++ Lab2/query4.sql
@@ -1,13 +1,4 @@
--- SELECT c.customerID, c.name
--- FROM Customers c
--- INNER JOIN Tickets t ON c.customerID = t.customerID
--- WHERE (c.name LIKE '%a%' OR c.name LIKE '%A%')
--- GROUP BY c.customerID
--- HAVING COUNT(t.customerID) >= 2
--- ORDER BY c.customerID ASC;
-
 SELECT DISTINCT c.customerID, c.name
--- SELECT *
 FROM Tickets t 
 INNER JOIN Showings s ON t.showingDate = s.showingDate AND t.startTime = s.startTime
 INNER JOIN Movies m ON s.movieID = m.movieID
--- Lab2/query5.sql
+++ Lab2/query5.sql
@@ -1,11 +1,3 @@
--- SELECT c.customerID AS "custID", c.name AS "custName", c.address AS "custAddress", 
--- th.address AS "theaterAddress", th.numSeats AS "theaterSeats", s.priceCode as "priceCode"
--- FROM Customers c, Theaters th, Showings s
--- INNER JOIN Tickets t ON c.customerID = t.customerID
--- WHERE c.name LIKE 'D%'
--- AND s.priceCode IS NOT NULL
--- AND t.showingDate BETWEEN '2019-06-01' AND '2019-06-30';
-
 SELECT c.customerID AS "custID", c.name AS "custName", c.address AS "custAddress", 
 th.address AS "theaterAddress", th.numSeats AS "theaterSeats", 
 s.priceCode as "priceCode"
