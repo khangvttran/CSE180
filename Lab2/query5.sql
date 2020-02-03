fd0b5369ae5170dd01b74915f5abcb16782887cd
Khang Tran <khvitran@ucsc.edu>
2020-02-02 19:10:06 -0800

    finished query 5

---
+++ Lab2/query5.sql
@@ -0,0 +1,19 @@
+-- SELECT c.customerID AS "custID", c.name AS "custName", c.address AS "custAddress", 
+-- th.address AS "theaterAddress", th.numSeats AS "theaterSeats", s.priceCode as "priceCode"
+-- FROM Customers c, Theaters th, Showings s
+-- INNER JOIN Tickets t ON c.customerID = t.customerID
+-- WHERE c.name LIKE 'D%'
+-- AND s.priceCode IS NOT NULL
+-- AND t.showingDate BETWEEN '2019-06-01' AND '2019-06-30';
+
+SELECT c.customerID AS "custID", c.name AS "custName", c.address AS "custAddress", 
+th.address AS "theaterAddress", th.numSeats AS "theaterSeats", 
+s.priceCode as "priceCode"
+FROM Tickets t 
+INNER JOIN Customers c ON t.customerID = c.customerID
+INNER JOIN Theaters th ON t.theaterID = th.theaterID
+INNER JOIN Showings s ON t.showingDate = s.showingDate
+WHERE c.name LIKE 'D%'
+AND (t.showingDate BETWEEN '2019-06-01' AND '2019-06-30')
+AND th.numSeats > 5
+AND s.priceCode IS NOT NULL;
