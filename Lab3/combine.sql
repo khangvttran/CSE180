7225164bba9c2b3ef0d841c73145502df9375430
Khang Tran <khvitran@ucsc.edu>
2020-02-21 23:59:19 -0800

    finished combine.sql

--- Lab3/combine.sql
+++ Lab3/combine.sql
@@ -3,3 +3,21 @@
 -- CSE180
 -- Lab3
 
+BEGIN TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+
+UPDATE Showings s
+    SET movieID = ms.movieID
+    FROM ModifyShowings ms 
+    WHERE ms.theaterID = s.theaterID
+    AND ms.showingDate = s.showingDate
+    AND ms.startTime = s.startTime;
+
+INSERT INTO Showings(theaterID, showingDate, startTime, movieID, priceCode)
+    SELECT ms.theaterID, ms.showingDate, ms.startTime, ms.movieID, NULL
+    FROM ModifyShowings ms
+    WHERE NOT EXISTS (SELECT * FROM Showings s
+                        WHERE ms.theaterID = s.theaterID
+                        AND ms.showingDate = s.showingDate
+                        AND ms.startTime = s.startTime);
+
+COMMIT;
