eb2e819a5859c8571460b11cf068e205698429db
Khang Tran <khvitran@ucsc.edu>
2020-02-22 22:43:22 -0800

    working on createview.sql, need to somehow replace NULL with 0

--- Lab3/createview.sql
+++ Lab3/createview.sql
@@ -2,3 +2,17 @@
 -- khvitran
 -- CSE180
 -- Lab3
+
+CREATE VIEW earningsView AS
+    SELECT m.movieID, SUM(ticketPrice) AS computedEarnings
+    -- CASE
+    --     WHEN ticketPrice IS NOT NULL
+    --     THEN ticketPrice
+    --     ELSE 0
+    -- END
+    FROM Movies m 
+    LEFT JOIN Showings s ON m.movieID = s.movieID
+    LEFT JOIN Tickets t ON s.theaterID = t.theaterID
+    AND s.showingDate = t.showingDate
+    AND s.startTime = t.startTime
+    GROUP BY m.movieID;
