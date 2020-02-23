050c37b94d12fedd2127f14bf1fb1e44c94480f8
Khang Tran <khvitran@ucsc.edu>
2020-02-22 16:12:48 -0800

    finished general.sql

--- Lab3/general.sql
+++ Lab3/general.sql
@@ -2,3 +2,11 @@
 -- khvitran
 -- CSE180
 -- Lab3
+
+ALTER TABLE Tickets ADD CONSTRAINT positiveTicketPrice CHECK (ticketPrice > 0);
+
+ALTER TABLE Customers ADD CHECK (joinDate >= '2015-11-27');
+
+ALTER TABLE Showings ADD CHECK (CASE 
+                                    WHEN movieID IS NOT NULL THEN priceCode IS NOT NULL 
+                                END);
